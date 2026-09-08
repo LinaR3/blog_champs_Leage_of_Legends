6698bb54ba4a3f635c417d80e253dd8c27ff46a5
Lina Reina <linareinap3@gmail.com>
2026-09-08 05:24:11 +0000

    Folders creados para integrar el servicion de la API  ddragon.js

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,8 @@ export const Home = () => {
 
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
+			<h1>Blog</h1>
+			<h1>CHAMPS_LOL</h1>
 			<p>
 				<img src={rigoImageUrl} />
 			</p>
